65a38ab6693440abca87c0feb4a490793abd1486
PovelitelSobak <55492409+PovelitelSobak@users.noreply.github.com>
2023-12-18 20:44:41 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ window.onload = () => {
   document.getElementById("button").onclick = () => {
     window.YaAuthSuggest.init(
       {
-        client_id: "c46f0c53093440c39f12eff95a9f2f93",
+        client_id: "53100541249d4b9480b880ba0005fd7d",
         response_type: "token",
-        redirect_uri: "https://examplesite.com/suggest/token",
+        redirect_uri: "https://oauth-master-class-gamma.vercel.app/token.html",
       },
-      "https://examplesite.com",
+      "https://oauth-master-class-gamma.vercel.app",
       {
         view: "button",
         parentId: "buttonContainer",
